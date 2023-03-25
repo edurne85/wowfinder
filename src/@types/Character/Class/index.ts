c42fe24e3352219fb87e49c3ae4d828eb255d17a
Edurne Pascual <herenvardo@gmail.com>
2023-03-25 12:08:24 +0100

    chore: reformat and clean Class.ts

--- src/@types/Character/Class/index.ts
+++ src/@types/Character/Class/index.ts
@@ -4,63 +4,81 @@ import { StatSet, statMod } from '../Stats';
 import type { Aura } from './Auras';
 import { ClassFeature } from './Features';
 
-type featureEntry = {level: number, feature: string};
-type auraEntry = {level: number, aura: Aura};
+type featureEntry = {
+    level: number;
+    feature: string
+};
+
+type auraEntry = {
+    level: number;
+    aura: Aura
+};
+
 type rawFeaturesEntry = featureEntry | auraEntry;
 
 interface ClassBuilder {
-    key: string,
-    hd: number,
-    bab: number,
-    fort: boolean,
-    refl: boolean,
-    will: boolean,
-    skl: number,
-    arc: number,
-    div: number,
-    esp: number,
-    wealth: number,
-    features: rawFeaturesEntry[],
-    skills: string[],
+    key: string;
+    hd: number;
+    bab: number;
+    fort: boolean;
+    refl: boolean;
+    will: boolean;
+    skl: number;
+    arc: number;
+    div: number;
+    esp: number;
+    wealth: number;
+    features: rawFeaturesEntry[];
+    skills: string[];
 }
 
 interface ClassBonuses {
-    hp: number,
-    bab: number,
+    hp: number;
+    bab: number;
     saves: {
-        fort: number,
-        refl: number,
-        will: number,
-    },
+        fort: number;
+        refl: number;
+        will: number;
+    };
     efl: {
-        arc: number,
-        div: number,
-        esp: number,
-    },
-    skillRanks: number,
-    classSkills: Set<Skill>,
-    features: {[key in ClassFeature]?: number},
+        arc: number;
+        div: number;
+        esp: number;
+    };
+    skillRanks: number;
+    classSkills: Set<Skill>;
+    features: { [key in ClassFeature]?: number };
 }
 
-type FeaturesList = {level: number, feature: ClassFeature}[];
-type AurasList = {level: number, aura: Aura}[];
+type FeaturesList = {
+    level: number;
+    feature: ClassFeature;
+}[];
+
+type AurasList = {
+    level: number;
+    aura: Aura;
+}[];
 
 const helpers = {
-    hdAvg: (hd: number): number => Math.ceil((hd+1) / 2),
+    hdAvg: (hd: number): number => Math.ceil((hd + 1) / 2),
     hdFirst: (hd: number): number => hd - helpers.hdAvg(hd),
     validSkills: new Set(Object.values(Skill)),
-    mapFeatures: (list: {level: number, feature?: string}[]): FeaturesList =>
+    mapFeatures: (list: { level: number; feature?: string }[]): FeaturesList =>
         list
             .filter(entry => entry.feature as ClassFeature)
-            .map(f => ({ level: f.level, feature: ClassFeature[f.feature as keyof typeof ClassFeature]})),
-    mapAuras: (list: {level: number, aura?: Aura}[]): AurasList => 
+            .map(f => ({
+                level: f.level,
+                feature: ClassFeature[f.feature as keyof typeof ClassFeature],
+            })),
+    mapAuras: (list: { level: number; aura?: Aura }[]): AurasList =>
         list
             .filter(entry => entry.aura as Aura)
-            .map(entry => ({level: entry.level, aura: entry.aura as Aura})),
+            .map(entry => ({ level: entry.level, aura: entry.aura as Aura })),
 };
 
-type Classes = {[key:string]: Class};
-type ClassLevels = { cls: Class, level: number }[];
+type Classes = { [key: string]: Class };
+type ClassLevels = { cls: Class; level: number }[];
 
 class Class {
     private _key: string;
@@ -113,7 +131,9 @@ class Class {
         );
     }
 
-    get key(): string { return this._key; }
+    get key(): string {
+        return this._key;
+    }
 
     features(level: number): ClassFeature[] {
         return this._features.filter(f => f.level <= level).map(f => f.feature);
@@ -149,24 +169,25 @@ class Class {
         if (classLevels.length > 0) {
             result.hp += helpers.hdFirst(classLevels[0].cls.hd);
         }
-        for (const {cls, level} of classLevels) {
+        for (const { cls, level } of classLevels) {
             result.hp += (helpers.hdAvg(cls.hd) + statMod(stats.CON)) * level;
             result.bab += cls.bab * level;
-            result.saves.fort += (cls.fort ? 1/2 : 1/3) * level;
+            result.saves.fort += (cls.fort ? 1 / 2 : 1 / 3) * level;
             goodSaves.fort ||= cls.fort;
-            result.saves.refl += (cls.refl ? 1/2 : 1/3) * level;
+            result.saves.refl += (cls.refl ? 1 / 2 : 1 / 3) * level;
             goodSaves.refl ||= cls.refl;
-            result.saves.will += (cls.will ? 1/2 : 1/3) * level;
+            result.saves.will += (cls.will ? 1 / 2 : 1 / 3) * level;
             goodSaves.will ||= cls.will;
             result.efl.arc += cls.arc * level;
             result.efl.div += cls.div * level;
             result.efl.esp += cls.esp * level;
             result.skillRanks += (cls.skl + statMod(stats.INT)) * level;
             for (const f of cls._features.filter(f => f.level <= level)) {
-                result.features[f.feature] = (result.features[f.feature] || 0) + 1;
+                result.features[f.feature] =
+                    (result.features[f.feature] || 0) + 1;
             }
             cls.skills.forEach((value: Skill) => result.classSkills.add(value));
-       }
+        }
         if (goodSaves.fort) {
             result.saves.fort += 2;
         }
@@ -195,15 +216,9 @@ class Class {
     private static _imported: Classes | null = null;
 
     static import(dir = window.Main.asset('Classes')): Classes {
-        return this._imported ||= forceDataImportKeyS(dir, this.build);
+        return (this._imported ||= forceDataImportKeyS(dir, this.build));
     }
 }
-export type {
-    Classes,
-    ClassLevels,
-    ClassBonuses,
-};
-export {
-    ClassFeature,
-    Class,
-};
+
+export type { Classes, ClassLevels, ClassBonuses };
+export { ClassFeature, Class };
