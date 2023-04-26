d919aa8666f54529267b8d99f36753ecf10cb21f
Edurne Pascual <herenvardo@gmail.com>
2023-04-26 23:29:10 +0200

    partial(332): Early implementation of bear and cat forms

--- src/types/Transformation/Shapeshift/base.ts
+++ src/types/Transformation/Shapeshift/base.ts
@@ -1,10 +1,32 @@
+import { CharacterOverride } from '../../Character/base/CharacterOverride';
 import { Character } from '../../Character';
-import { Bonus } from '../../Character/Bonus';
 import { Transformation } from '../base';
 
+interface ShapeshiftBuilder {
+    rank: number;
+}
 abstract class Shapeshift implements Transformation {
-    abstract compute(base: Character, rank?: number): Bonus;
-    abstract apply(base: Character): Character;
+    #rank: number;
+
+    protected constructor({ rank }: ShapeshiftBuilder) {
+        this.#rank = rank;
+    }
+
+    get rank(): number {
+        return this.#rank;
+    }
+
+    abstract compute(base: Character, rank: number): CharacterOverride;
+
+    apply(base: Character): Character {
+        base.setOverride(this.compute(base, this.#rank));
+        return base;
+    }
+
+    static defaultSize(rank: number): number {
+        return rank < 3 ? 0 : (rank - 1) / 2;
+    }
 }
 
 export { Shapeshift };
+export type { ShapeshiftBuilder };
