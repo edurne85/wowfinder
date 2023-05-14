81029030ec4db908243dced5bfcbfc18700daf4d
Edurne Pascual <herenvardo@gmail.com>
2023-05-14 17:17:50 +0200

    chore: version bump

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './i18n';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+if (!container) {
+    throw new Error('Root element not found');
+}
+const root = createRoot(container);
+root.render(<App />);
