a4138dbfd049e25ec8475027d731e5167481d398
Edurne Pascual <herenvardo@gmail.com>
2023-06-10 12:00:25 +0200

    partial(547) model and builder for alchemy potions

--- src/types/Item/Consumable/builder.ts
+++ src/types/Item/Consumable/builder.ts
@@ -1,11 +1,13 @@
 import { builder } from '../../../utils';
 import { Item } from '../base';
+import { Potion } from './Potion';
 import { SpellPotion, SpellScroll, Wand } from './SpellContainer';
 
 const consumableBuilderByTypeKey: { [key: string]: builder<Item> } = {
     SpellPotion: SpellPotion.build,
     SpellScroll: SpellScroll.build,
     SpellWand: Wand.build,
+    Potion: Potion.build,
 };
 
 export { consumableBuilderByTypeKey };
