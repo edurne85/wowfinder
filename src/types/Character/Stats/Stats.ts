2ac7265ddd4156b994763981dbd1cc3b9b11d83a
Edurne Pascual <herenvardo@gmail.com>
2023-06-13 01:14:57 +0200

    chore(404): more Stats refactor

--- src/types/Character/Stats/Stats.ts
+++ src/types/Character/Stats/Stats.ts
@@ -1,51 +1,16 @@
 import { Mass } from '../../Units';
+import { StatsBase } from './StatsBase';
 import {
     StatSet,
-    baseDefault,
-    zeroDefault,
     PartialStatBlock,
     addStatSets,
     statMod,
     carry,
 } from './helpers';
 
-class Stats {
-    #base: StatSet;
-    #racial: StatSet;
-    #enhance: StatSet;
-    #gear: StatSet;
-    #misc: StatSet;
-    #temp: StatSet;
-
-    constructor({
-        base = baseDefault,
-        racial = zeroDefault,
-        enhance = zeroDefault,
-        gear = zeroDefault,
-        misc = zeroDefault,
-        temp = zeroDefault,
-    }: PartialStatBlock) {
-        this.#base = base;
-        this.#racial = racial;
-        this.#enhance = enhance;
-        this.#gear = gear;
-        this.#misc = misc;
-        this.#temp = temp;
-    }
-
-    get totals(): StatSet {
-        return addStatSets(
-            this.#base,
-            this.#racial,
-            this.#enhance,
-            this.#gear,
-            this.#misc,
-            this.#temp,
-        );
-    }
-
+class Stats extends StatsBase {
     get active(): StatSet {
-        return addStatSets(this.#base, this.#racial, this.#enhance);
+        return addStatSets(this.base, this.racial, this.enhance);
     }
 
     get totalMods(): StatSet {
@@ -58,30 +23,6 @@ class Stats {
             WIS: statMod(totals.WIS),
             CHA: statMod(totals.CHA),
         };
-    }
-
-    get base(): StatSet {
-        return Object.assign({}, this.#base);
-    }
-
-    get racial(): StatSet {
-        return Object.assign({}, this.#racial);
-    }
-
-    get enhance(): StatSet {
-        return Object.assign({}, this.#enhance);
-    }
-
-    get gear(): StatSet {
-        return Object.assign({}, this.#gear);
-    }
-
-    get misc(): StatSet {
-        return Object.assign({}, this.#misc);
-    }
-
-    get temp(): StatSet {
-        return Object.assign({}, this.#temp);
     }
 
     get carry(): Mass {
