4a61d9806e0dcaa8ee856bf66f8622894c35c6f3
Edurne Pascual <herenvardo@gmail.com>
2024-02-20 19:38:48 +0100

    chore(#911): Validation and multiplication methods for ResistBonus

--- src/types/Character/Bonus/ResistBonus.ts
+++ src/types/Character/Bonus/ResistBonus.ts
@@ -1,23 +1,24 @@
+import { Validable, validateNumber } from '@model/Validable';
 import { sum } from '../../../utils';
 import { DamageType } from '../../Damage';
 import type { ResistancePartialSet, ResistanceSet } from '../Resistances';
 
 function fill(data: ResistancePartialSet, filler = 0): ResistanceSet {
     return {
-        bludgeoning: data.bludgeoning || filler,
-        slashing: data.slashing || filler,
-        piercing: data.piercing || filler,
-        arcane: data.arcane || filler,
-        fire: data.fire || filler,
-        cold: data.cold || filler,
-        nature: data.nature || filler,
-        shadow: data.shadow || filler,
-        holy: data.holy || filler,
-        psychic: data.psychic || filler,
+        bludgeoning: data.bludgeoning ?? filler,
+        slashing: data.slashing ?? filler,
+        piercing: data.piercing ?? filler,
+        arcane: data.arcane ?? filler,
+        fire: data.fire ?? filler,
+        cold: data.cold ?? filler,
+        nature: data.nature ?? filler,
+        shadow: data.shadow ?? filler,
+        holy: data.holy ?? filler,
+        psychic: data.psychic ?? filler,
     };
 }
 
-export default class ResistBonus {
+export default class ResistBonus implements Validable {
     #values: ResistanceSet;
 
     constructor(data: ResistancePartialSet) {
@@ -30,6 +31,16 @@ export default class ResistBonus {
 
     get isZero(): boolean {
         return Object.values(this.#values).every(v => v === 0);
+    }
+
+    validate(): void {
+        Object.keys(DamageType).forEach(type => {
+            validateNumber(this.#values[type as DamageType]);
+        });
+    }
+
+    static validate(bonus: ResistBonus): asserts bonus is ResistBonus {
+        bonus.validate();
     }
 
     static get zero(): ResistBonus {
@@ -46,6 +57,15 @@ export default class ResistBonus {
         return result;
     }
 
+    static multiply(bonus: ResistBonus, factor: number): ResistBonus {
+        const result = this.zero;
+        for (const type of Object.keys(DamageType)) {
+            result.#values[type as DamageType] =
+                bonus.#values[type as DamageType] * factor;
+        }
+        return result;
+    }
+
     static max(...args: ResistBonus[]): ResistBonus {
         const result = this.zero;
         for (const type of Object.keys(DamageType)) {
@@ -57,7 +77,7 @@ export default class ResistBonus {
     }
 
     static build(raw: any = {}): ResistBonus {
-        return new ResistBonus(Object.assign({}, this.zero.#values, raw));
+        return new ResistBonus({ ...this.zero.#values, ...raw });
     }
 }
 
