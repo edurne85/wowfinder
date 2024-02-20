7c61085315cf4597adfae79e7dc8d63623a0c226
Edurne Pascual <herenvardo@gmail.com>
2024-02-20 19:41:00 +0100

    chore(#911): Validation and multiplication methods for SkillsBonus

--- src/types/Character/Bonus/Skills.ts
+++ src/types/Character/Bonus/Skills.ts
@@ -1,7 +1,8 @@
 import { Skill, SkillSet } from '../Skill';
 import { sum } from '../../../utils';
+import { Validable, validateNumber } from '@model/Validable';
 
-export default class SkillsBonus {
+export default class SkillsBonus implements Validable {
     #values: SkillSet;
 
     constructor(values: SkillSet) {
@@ -10,13 +11,13 @@ export default class SkillsBonus {
             Object.defineProperty(this, skill, {
                 enumerable: true,
                 configurable: false,
-                get: () => this.#values[skill as Skill] || 0,
+                get: () => this.#values[skill as Skill] ?? 0,
             });
         }
     }
 
     value(skill: Skill): number {
-        return this.#values[skill] || 0;
+        return this.#values[skill] ?? 0;
     }
 
     get values(): SkillSet {
@@ -31,12 +32,33 @@ export default class SkillsBonus {
         return new SkillsBonus({});
     }
 
+    validate(): void {
+        Object.keys(Skill).forEach(skill => {
+            validateNumber(this.#values[skill as Skill]);
+        });
+    }
+
+    static validate(bonus: SkillsBonus): void {
+        bonus.validate();
+    }
+
     static sum(...args: SkillsBonus[]): SkillsBonus {
         const result: SkillSet = {};
         for (const skill of Object.keys(Skill)) {
-            const value = sum(...args.map(s => s.#values[skill as Skill] || 0));
+            const value = sum(...args.map(s => s.#values[skill as Skill] ?? 0));
             if (value) {
                 result[skill as Skill] = value;
+            }
+        }
+        return new SkillsBonus(result);
+    }
+
+    static multiply(bonus: SkillsBonus, factor: number): SkillsBonus {
+        const result: SkillSet = {};
+        for (const skill of Object.keys(Skill)) {
+            const value = bonus.#values[skill as Skill] ?? 0;
+            if (value) {
+                result[skill as Skill] = value * factor;
             }
         }
         return new SkillsBonus(result);
@@ -46,7 +68,7 @@ export default class SkillsBonus {
         const result: SkillSet = {};
         for (const skill of Object.keys(Skill)) {
             const value = Math.max(
-                ...args.map(s => s.#values[skill as Skill] || 0),
+                ...args.map(s => s.#values[skill as Skill] ?? 0),
             );
             if (value) {
                 result[skill as Skill] = value;
